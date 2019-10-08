1c67d2ed0c7e4d3484fa342d25cbca4ae974614a
Menencia <menencia@gmail.com>
2019-10-08 23:06:19 +0200

    global redirection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GameService } from './game.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { GameService } from './game.service';
 export class AppComponent {
   title = 'clickingff7';
 
-  constructor(public game: GameService) {
+  constructor(
+    public game: GameService,
+    public router: Router
+  ) {
+    // Global redirection
+    if (this.game.isConnected()) {
+      this.router.navigateByUrl('/dashboard');
+    } else {
+      this.router.navigateByUrl('/home');
+    }
   }
 }
