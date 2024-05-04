bad6b4d648aaffe6e8e9ca171f94eb0672b88da2
Menencia <lucien.stojcevski@gmail.com>
2024-05-04 04:03:37 +0200

    Dev: Remove forgotten fdescribe

--- src/app/shared/utils/version.utils.spec.ts
+++ src/app/shared/utils/version.utils.spec.ts
@@ -1,7 +1,7 @@
 import { compareVersions } from './version.utils';
 
-fdescribe('Version Utils', () => {
-  it('should test compareVersions function', () => {
+describe('Version Utils', () => {
+  it('should test compareVersions', () => {
     expect(compareVersions('1.1.0', '1.2.0')).toBe(false);
     expect(compareVersions('1.2.0', '1.2.0')).toBe(true);
     expect(compareVersions('1.3.0', '1.2.0')).toBe(true);
