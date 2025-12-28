2f09b3d8935571b1c039e96f31a228a2e3b02047
Menencia <menencia@gmail.com>
2025-12-28 23:21:48 +0100

    add theme service

---
+++ src/app/shared/services/theme.service.ts
@@ -0,0 +1,26 @@
+import { DOCUMENT, Inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  theme?: string;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      this.applyTheme(theme);
+    }
+  }
+
+  applyTheme(theme: string | undefined) {
+    this.theme = theme;
+    if (this.theme) {
+      this.document.documentElement.setAttribute('data-theme', this.theme);
+      localStorage.setItem('theme', this.theme);
+    } else {
+      this.document.documentElement.removeAttribute('data-theme');
+      localStorage.removeItem('theme');
+    }
+  }
+}
--- src/app/views/view-config/view-config.component.ts
+++ src/app/views/view-config/view-config.component.ts
@@ -4,33 +4,43 @@ import { TranslateModule } from '@ngx-translate/core';
 import { UiLayoutDefaultComponent } from '@shared/components/ui-layout-default/ui-layout-default.component';
 import { LangService } from '@shared/services/lang.service';
 import { PlayerService } from '@shared/services/player.service';
+import { ThemeService } from '@shared/services/theme.service';
 import { Difficulty } from 'src/app/shared/interfaces/difficulty';
 
 @Component({
   selector: 'app-view-config',
   imports: [UiLayoutDefaultComponent, TranslateModule, FormsModule],
   templateUrl: './view-config.component.html',
-  styleUrls: ['./view-config.component.scss'],
 })
 export class ViewConfigComponent {
   difficulty: Difficulty;
-
   language: string;
+  theme: string;
 
   constructor(
     private playerService: PlayerService,
     private langService: LangService,
+    private themeService: ThemeService,
   ) {
     this.difficulty = this.playerService.difficulty;
     this.language = this.playerService.language;
+    this.theme = this.themeService.theme ?? 'browser';
   }
 
-  changeDifficulty(): void {
+  changeDifficulty() {
     this.playerService.difficulty = this.difficulty;
   }
 
-  changeLanguage(): void {
+  changeLanguage() {
     this.playerService.language = this.language;
     this.langService.useLang(this.playerService.language);
+  }
+
+  changeTheme() {
+    if (this.theme === 'browser') {
+      this.themeService.applyTheme(undefined);
+    } else {
+      this.themeService.applyTheme(this.theme);
+    }
   }
 }
