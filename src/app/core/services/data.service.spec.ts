4d44e520d577775a69827eb1e4c9f60a608a7491
Menencia <menencia@gmail.com>
2026-09-01 01:12:11 +0200

    chore: update to angular 18

--- src/app/core/services/data.service.spec.ts
+++ src/app/core/services/data.service.spec.ts
@@ -1,4 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
@@ -8,7 +11,8 @@ describe('DataService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [],
+      providers: [provideHttpClient(withInterceptorsFromDi())],
     });
     service = TestBed.inject(DataService);
   });
--- src/app/core/services/store.service.spec.ts
+++ src/app/core/services/store.service.spec.ts
@@ -1,4 +1,7 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
@@ -8,7 +11,8 @@ describe('StoreService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [],
+      providers: [provideHttpClient(withInterceptorsFromDi())],
     });
     service = TestBed.inject(StoreService);
   });
